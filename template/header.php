a30959bbd718e944f4e75a83df6cf716f1a4f371
LeNgocPhuc <lengocphuclnp90@gmail.com>
2023-05-16 12:17:33 +0700

    v4

--- template/header.php
+++ template/header.php
@@ -1,14 +1,8 @@
 <div class="bg-primary position-relative pt-5">
   <div class="overlay-gradient"></div>
   <div class="container position-relative">
-  	<div class="row pt-5">
-    <p class="text-justify">
-      Mã QR là một mã vạch hai chiều, bao gồm các điểm ảnh đen và trắng, cho phép mã hóa lên đến vài trăm ký tự. 
-      Ngày nay, các máy điện thoại thông minh và máy tính bảng có thể nhận biết và giải mã chúng cực kỳ nhanh – QR là đại diện cho từ Quick Response.
-      Do các dòng điện thoại thông minh ngày càng được dùng nhiều, nên mã QR cũng được sử dụng cho mục đích Marketing rộng rãi hơn. 
-      Các nhà quảng cáo được hưởng lợi từ mã QR bằng cách thêm nội dung digital như Websites, Videos, PDFs, 
-      hình ảnh hoặc chi tiết liên lạc trên tờ rơi, áp phích, danh mục sản phẩm và danh thiếp.
-    </p>
+  	<div class="row py-5 px-2 d-flex justify-content-center">
+    <h2>Thiết kế mã QR Online - Miễn phí - Đơn Giản Nhất</h2>
     </div>
     </div>
   </div>
